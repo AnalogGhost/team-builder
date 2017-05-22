6aa28ea63aa0fd6b8e5ca4f75073000930de1cc3
Mathew Brown <matt@mattbrown.com>
2017-05-22 09:49:10 -0600

    Create basic express server and stub out cohorts api route

--- index.js
+++ index.js
@@ -1,13 +1,29 @@
 'use strict';
 
-const http = require('http');
+const express = require('express');
+const app = express();
+
+//BodyParser
+const bodyParser = require('body-parser');
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
+
+//API Routes
+const cohorts = require('./routes/api/cohorts');
+
+app.use('/api/cohorts/', cohorts);
+
+//Static Files
+app.use(express.static('public'));
+
 const port = process.env.PORT || 3000;
-
-const server = http.createServer(function(req, res) {
-  res.setHeader('Content-Type', 'text/plain');
-  res.end('group-builder coming soon');
-});
-
-server.listen(port, function() {
+app.listen(port, () => {
   console.log('Listening on port', port);
 });
+
+process.on('uncaughtException', function(err) {
+    console.log( " UNCAUGHT EXCEPTION " );
+    console.log( "[Inside 'uncaughtException' event] " + err.stack || err.message );
+});
+
+module.exports = app;
