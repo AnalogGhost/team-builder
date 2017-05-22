6d72f07b3ca569596cf4672703a10ff33674e464
Mathew Brown <matt@mattbrown.com>
2017-05-22 14:41:50 -0600

    Add api route for cohorts

--- routes/api/cohorts.js
+++ routes/api/cohorts.js
@@ -1,24 +1,40 @@
 const router = require('express').Router();
-const knex = require('../../knex');
+const cohorts = require('../../queries/cohorts');
+
+const ev = require('express-validation');
+const validations = require('../../validations/cohorts');
 
 router.get('/', (req,res,next) => {
-  res.send('GET ALL ROUTE');
+  cohorts.list().then(result => {
+    res.send(result);
+  });
 });
 
-router.get('/:id', (req,res,next) => {
-  res.send('GET ROUTE FOR ID' + req.params.id);
+router.get('/:id', ev(validations.get), (req,res,next) => {
+  cohorts.get(req.params.id).then(result => {
+    if (!result) {
+      return res.sendStatus(404);
+    }
+    res.send(result);
+  });
 });
 
-router.post('/', (req,res,next) => {
-  res.send('POST ROUTE');
+router.post('/', ev(validations.post), (req,res,next) => {
+  cohorts.create(req.body).then(() => {
+    res.sendStatus(201);
+  });
 });
 
-router.patch('/:id', (req,res,next) => {
-  res.send('PATCH ROUTE');
+router.patch('/:id', ev(validations.patch), (req,res,next) => {
+  cohorts.update(req.params.id,req.body).then(() => {
+    res.sendStatus(204);
+  });
 });
 
-router.delete('/:id', (req,res,next) => {
-  res.send('DELETE ROUTE');
+router.delete('/:id', ev(validations.delete), (req,res,next) => {
+  cohorts.remove(req.params.id).then(() => {
+    res.sendStatus(204);
+  });
 });
 
 module.exports = router;
